9dd16db9d39ee322cb24d25e3490eaa1a99aa3bf
Lawrence Patrick Calulo <lugkhast@gmail.com>
2012-01-01 23:32:12 +0800

    Created reeaaaally simple mockup of schedule display concept

--- static/scripts/schedules-page/canvastest.js
+++ static/scripts/schedules-page/canvastest.js
@@ -1,24 +1,52 @@
 
+var GRID_COL_WIDTH = 120;
+var X_OFFSET = 45.5;
+
 function canvas_main() {
     var canvas = document.getElementById('tutorial');
     var ctx = canvas.getContext('2d');
+    var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     // Draw vertical lines
-    for (var i = 20; i <= 620; i += 100) {
+    for (var i = 0; i <= days.length; i++) {
         ctx.beginPath();
-        ctx.moveTo(i + 0.5, 20);
-        ctx.lineTo(i + 0.5, 420);
+        ctx.moveTo((i * GRID_COL_WIDTH) + X_OFFSET, 20);
+        ctx.lineTo((i * GRID_COL_WIDTH) + X_OFFSET, 520)
         ctx.stroke();
     }
 
     // Draw header border lines
     for (var i = 0; i <= 32; i += 32) {
         ctx.beginPath()
-        ctx.moveTo(20.5, i + 20.5);
-        ctx.lineTo(620.5, i + 20.5);
+        ctx.moveTo(X_OFFSET, i + 20.5);
+        ctx.lineTo(X_OFFSET + (GRID_COL_WIDTH * days.length), i + 20.5);
         ctx.stroke();
     }
+
+    ctx.beginPath()
+    ctx.moveTo(0, 20.5);
+    ctx.lineTo(X_OFFSET, 20.5);
+    ctx.stroke();
+
+    // Draw text
+    ctx.font = '12pt Droid Sans';
+    for (var i = 0; i < days.length; i++) {
+        ctx.fillText(days[i], X_OFFSET + 10 + (GRID_COL_WIDTH * i), 42);
+    }
+
+    ctx.font = '8pt Droid Sans';
+    ctx.textAlign = 'right';
+    ctx.fillText("8:00am", 43, 57);
+    ctx.fillText("9:00am", 43, 97);
+    ctx.fillText("10:00am", 43, 137);
+    ctx.fillText("11:00am", 43, 177);
+    ctx.fillText("12:00nn", 43, 217);
+    ctx.fillText("1:00pm", 43, 257);
+    ctx.fillText("2:00pm", 43, 297);
+    ctx.fillText("3:00pm", 43, 337);
+    ctx.fillText("4:00pm", 43, 377);
+    ctx.fillText("5:00pm", 43, 417);
+    ctx.fillText("6:00pm", 43, 457);
 }
 
 $(document).ready(canvas_main);
-
