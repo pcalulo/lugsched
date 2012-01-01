4628407af1d8e5ebd70f652c086d7e5320cc5a87
Lawrence Patrick Calulo <lugkhast@gmail.com>
2012-01-01 22:52:07 +0800

    Working on a grid for the new-style schedule display

--- static/scripts/schedules-page/canvastest.js
+++ static/scripts/schedules-page/canvastest.js
@@ -3,11 +3,21 @@ function canvas_main() {
     var canvas = document.getElementById('tutorial');
     var ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'rgb(200, 0, 0';
-    ctx.fillRect(10, 10, 55, 50);
+    // Draw vertical lines
+    for (var i = 20; i <= 620; i += 100) {
+        ctx.beginPath();
+        ctx.moveTo(i + 0.5, 20);
+        ctx.lineTo(i + 0.5, 420);
+        ctx.stroke();
+    }
 
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 55, 50);
+    // Draw header border lines
+    for (var i = 0; i <= 32; i += 32) {
+        ctx.beginPath()
+        ctx.moveTo(20.5, i + 20.5);
+        ctx.lineTo(620.5, i + 20.5);
+        ctx.stroke();
+    }
 }
 
 $(document).ready(canvas_main);
