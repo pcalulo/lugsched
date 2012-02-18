af0729d214420a41d2b67666a7ddd1fb3eecf96f
Lawrence Patrick Calulo <lugkhast@gmail.com>
2012-02-18 10:43:51 +0800

    User must be logged in to get to /schedules/

--- schedules/views.py
+++ schedules/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def sched_home(request):
 	template = loader.get_template('schedules/schedules.html')
 	context = RequestContext(request, {
