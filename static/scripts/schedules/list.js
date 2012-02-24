bf19c61206bab7b1fde79bd653268ec566d0dcdb
Lawrence Patrick Calulo <lugkhast@gmail.com>
2012-02-24 14:54:23 +0800

    Attempted to make the schedules page more mobile-friendly

--- static/scripts/schedules/list.js
+++ static/scripts/schedules/list.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
     var i, clist, item;
     clist = new ClickList($(".list-test"));
 
+    /*
     for (i = 0; i < 5; i++) {
        item = new ClickListElement()
        item.mainText = "Test Item " + i
        item.subText = "Hello, World!";
        clist.add(item);
     }
+    */
 
     var sdisp = new ScheduleDisplay(document.getElementById("schedule-canvas"));
 });
