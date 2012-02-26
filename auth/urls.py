6c4cfbadd76967bc8e1d7c92558b285d4c122469
Lawrence Patrick Calulo <lugkhast@gmail.com>
2012-02-26 20:00:10 +0800

    Login page now redirects to /schedules/ if the user is already logged in

--- auth/urls.py
+++ auth/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
 from views import *
-from django.contrib.auth.views import login, logout_then_login
 
 urlpatterns = patterns('',
     url(r'register/$', register, name='register'),
-    url(r'login/$', login, {'template_name': 'auth/login.html'}),
+    # url(r'login/$', login, {'template_name': 'auth/login.html'}),
+    url(r'login/$', user_login),
 
     # It *might* be more likely that when a user logs out, he's simply letting
     # a friend log in.
--- auth/views.py
+++ auth/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import login
 from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
@@ -14,6 +15,12 @@ def register(request):
         form = UserCreationForm()
 
     return render_to_response('auth/register.html', RequestContext(request, {'form': form, }))
+
+def user_login(request):
+    if request.user.is_authenticated():
+        return redirect('/schedules/')
+    
+    return login(request, template_name='auth/login.html')
 
 def user_logout(request):
     logout(request)
