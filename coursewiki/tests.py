7c7e0708b001a3c7927c8f4bdaeca1c4b3774356
Lawrence Patrick Calulo <lugkhast@gmail.com>
2012-05-23 18:44:11 +0800

    Add test for ArchivedUniversity.copy
     - To do this sanely, the sample data has been refactored, with its code
       being moved into functions for convenient importing.

--- coursewiki/tests.py
+++ coursewiki/tests.py
@@ -1,16 +1,52 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Tests for the Course Wiki
 """
 
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+from coursewiki.models import *
+from core.models import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+import sample_data
+
+class CopyTest(TestCase):
+    """
+    Tests that check whether the data that gets copied into the archives is
+    identical to the copy in core
+    """
+
+    def setUp(self):
+        # Create the test user
+        self.user = user = User()
+        user.username = 'Tester'
+        user.email = 'test@example.com'
+        user.save()
+        sample_data.add_wiki_data(user)
+
+
+    def test_university(self):
+        universities = University.objects.all()
+
+        uni = universities[0]
+        orig_name = uni.name
+        orig_address = uni.address
+
+        appended_string = 'Testing'
+
+        uni.name = uni.name + appended_string
+        uni.address = uni.address + appended_string
+        uni.update(self.user, appended_string)
+
+        # The update() calls are extremely close to each other, so we can't
+        # order by edit_timestamp
+        history = uni.archiveduniversity_set.all().order_by('-pk')
+
+        latest = history[0]
+        self.assertEqual(latest.name, orig_name + appended_string)
+        self.assertEqual(latest.address, orig_address + appended_string)
+
+        second_latest = history[1]
+        self.assertEqual(second_latest.name, orig_name)
+        self.assertEqual(second_latest.address, orig_address)
+
--- sample_data.py
+++ sample_data.py
@@ -2,65 +2,86 @@
 # Note: run this like so:
 # ./manage.py shell < sample_data.py
 
+# The tests may import this file to use the add_wiki_data method. To keep that
+# code from exploding, keep all code inside methods.
+
+# Also take note that, as this file is intended to be piped into the Python
+# interactive interpreter, do not put empty lines inside code for methods, as
+# this makes the interactive interpreter think that you are done with the
+# method.
+
 from datetime import datetime, time
 from core.models import *
 from django.contrib.auth.models import User
 
-uni_count = len(University.objects.all())
-if (uni_count > 0):
-    print 'Found at least one existing University!'
-    print 'This script should only be used to quickly set up an empty database'
-    sys.exit(1)
 
-user_count = len(User.objects.all())
-if (user_count == 0):
-    print 'No users found!'
-    print 'Please configure a superuser - The user with ID 0 will be assumed to be the admin'
-    sys.exit(1)
+def do_checks():
+    uni_count = len(University.objects.all())
+    if (uni_count > 0):
+        print 'Found at least one existing University!'
+        print 'This script should only be used to quickly set up an empty database'
+        sys.exit(1)
+    user_count = len(User.objects.all())
+    if (user_count == 0):
+        print 'No users found!'
+        print 'Please configure a superuser - The user with ID 0 will be assumed to be the admin'
+        sys.exit(1)
+    # Get the first user, which we assume to be the superuser. The sample data will
+    # be attributed to this user.
+    user = User.objects.all()[0]
 
-# Get the first user, which we assume to be the superuser. The sample data will
-# be attributed to this user.
-user = User.objects.all()[0]
 
-uni = University()
-uni.name = 'De La Salle University'
-uni.address = '2401 Taft Avenue, Malate, Manila'
-uni.update(user, 'Added sample data')
+def add_wiki_data(user):
+    if not user.id:
+        raise ValueError('User passed in has not been saved to the database yet')
+    uni = University()
+    uni.name = 'De La Salle University'
+    uni.address = '2401 Taft Avenue, Malate, Manila'
+    uni.update(user, 'Added sample data')
+    term = Term()
+    term.university = uni
+    term.academic_year = 2011
+    term.index = 1
+    term.note = 'Hello, world!'
+    term.update(user, 'Added sample data')
+    course = Course()
+    course.code = 'EXAMPLE'
+    course.name = 'An example course'
+    course.description = 'The quick brown fox jumps over the lazy dog'
+    course.university = uni
+    course.creation_date = datetime.now()
+    course.update(user, 'Added sample data')
+    section = Section()
+    section.name = 'S22'
+    section.course = course
+    section.term = term
+    section.update(user, 'Added sample data')
+    meeting = Meeting()
+    meeting.section = section
+    meeting.start_time = time(8, 0)
+    meeting.end_time = time(9, 30)
+    meeting.has_mondays = True
+    meeting.has_thursdays = True
+    meeting.update(user, 'Added sample data')
 
-term = Term()
-term.university = uni
-term.academic_year = 2011
-term.index = 1
-term.note = 'Hello, world!'
-term.update(user, 'Added sample data')
 
-course = Course()
-course.code = 'EXAMPLE'
-course.name = 'An example course'
-course.description = 'The quick brown fox jumps over the lazy dog'
-course.university = uni
-course.creation_date = datetime.now()
-course.update(user, 'Added sample data')
+def create_profile_for_user(user):
+    # Create the user's profile
+    # The university must exist before we can create this
+    profile = UserProfile()
+    profile.user = user
+    profile.nickname = 'Pat'
+    profile.university = University.objects.all()[0]
+    profile.save()
 
-section = Section()
-section.name = 'S22'
-section.course = course
-section.term = term
-section.update(user, 'Added sample data')
 
-meeting = Meeting()
-meeting.section = section
-meeting.start_time = time(8, 0)
-meeting.end_time = time(9, 30)
-meeting.has_mondays = True
-meeting.has_thursdays = True
-meeting.update(user, 'Added sample data')
+def main():
+    do_checks()
+    user = User.objects.all()[0]
+    add_wiki_data(user)
+    create_profile_for_user(user)
 
-# Create the user's profile
-# The university must exist before we can create this
-profile = UserProfile()
-profile.user = user
-profile.nickname = 'Pat'
-profile.university = uni
-profile.save()
+# Check if we're being piped into manage.py
+if __name__ == '__builtin__':
+    main()
 
